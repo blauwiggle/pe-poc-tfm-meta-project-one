1d396a41456cb6b0f444d16c5046f819c4fa8dc4
Michael Vanhee <michael.vanhee@ktm.com>
2024-03-28 13:12:03 +0100

    x

--- {{cookiecutter.module_name}}/main.tf
+++ {{cookiecutter.module_name}}/main.tf
@@ -1,12 +1,10 @@
 locals {
   groups_config = [
     {
-      group_subject     = "{{cookiecutter.group_subject}}"
-      group_description = "{{cookiecutter.group_description}}"
-      owners            = ["{{cookiecutter.owners}}"]
-      {% for member in cookiecutter.members %}
-      members           = ["{{member}}"]
-      {% endfor %}
+      group_subject     = "{{ cookiecutter.group_subject }}"
+      group_description = "{{ cookiecutter.group_description }}"
+      owners            = [{{ cookiecutter.owners | map(attribute='id') | join(', ') }}]
+      members           = [{{ cookiecutter.members | map(attribute='id') | join(', ') }}]
     }
   ]
 }
