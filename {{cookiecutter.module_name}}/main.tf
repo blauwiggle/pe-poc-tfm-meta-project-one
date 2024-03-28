b048b15a8d04a0dff6c86b412f5af647098c6ffb
Michael Vanhee <michael.vanhee@ktm.com>
2024-03-28 13:49:15 +0100

    x

--- {{cookiecutter.module_name}}/main.tf
+++ {{cookiecutter.module_name}}/main.tf
@@ -4,14 +4,9 @@ locals {
     {
       group_subject     = "{{ details.group_subject }}"
       group_description = "{{ details.group_description }}"
-
-      {% for owner in details.owners -%}
-      owners            = [{{ owner }}]
-      {% endfor -%}
-
-      {% for member in details.members -%}
-      members           = [{{ member }}]
-      {% endfor -%}
-    }
+      owners            = [{% for owner in details.owners %}"{{ owner }}"{% if not loop.last %}, {% endif %}{% endfor %}]
+      members           = [{% for member in details.members %}"{{ member }}"{% if not loop.last %}, {% endif %}{% endfor %}]
+    },{% if not loop.last %},{% endif %}
     {% endfor %}
   ]
+}
