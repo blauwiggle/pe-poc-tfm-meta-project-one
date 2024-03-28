7f44b0c6fc45bafdbee1dfe28e6046b60b247748
Michael Vanhee <michael.vanhee@ktm.com>
2024-03-28 12:44:52 +0100

    x

--- {{cookiecutter.module_name}}/main.tf
+++ {{cookiecutter.module_name}}/main.tf
@@ -1,10 +1,10 @@
 locals {
   groups_config = [
     {
-      group_subject     = {{cookiecutter.group_subject}}
-      group_description = {{cookiecutter.group_description}}
-      owners            = {{cookiecutter.owners}}
-      members           = {{cookiecutter.members}}
+      group_subject     = "{{cookiecutter.group_subject}}"
+      group_description = "{{cookiecutter.group_description}}"
+      owners            = ["{{cookiecutter.owners}}"]
+      members           = ["{{cookiecutter.members}}"]
     }
   ]
 }
